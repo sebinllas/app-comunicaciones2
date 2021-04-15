012da942f2774bf5935c1ddb26ebc4a3979623a4
Sebastian <sebinllas13@gmail.com>
2021-04-15 09:09:41 -0500

    quitando dirección

--- public/app.js
+++ public/app.js
@@ -31,7 +31,7 @@ canvas.setAttribute("height", 400);
 var ctx = canvas.getContext("2d");
 
 //----------------------Sockets Handling----------------------
-const socket = io("https://app-comunicaciones2.herokuapp.com:3000");
+const socket = io();
 
 socket.on("firstConnection", (data) => {
   console.log(data);
