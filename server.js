3492bb96e0daf61f9d004728d7240bac2387ac3c
Sebastian <sebinllas13@gmail.com>
2021-04-20 21:42:51 -0500

    volviendo al cors anterior

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.listen(app.get("port"), () => {
 // const io = require('socket.io')("https://app-comunicaciones2.herokuapp.com");
 const io = require("socket.io")(process.env.PORT || 3000, {
   cors: {
-    origin: "https://paintgame.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST"],
     withCredentials: false,
   },
